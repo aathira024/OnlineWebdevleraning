9a147f14ec70f124f6420f23275b57c4dc2bd984
aathira.024 <aathra.024@gmail.com>
2022-11-27 15:37:10 +0530

    own projects in web development

---
+++ my projects/js from youtube/VARIBLES/script.js
@@ -0,0 +1,38 @@
+
+//string
+var person1 ="ammu";
+var persson2="kannu";
+document.write(person1);
+document.write("<br>");
+document.write(typeof person1);
+//number
+var num1 =20;
+var num2=30;
+document.write(num1);
+document.write("<br>");
+document.write(typeof num1);
+//boolean
+var jas =true;
+var html = false;
+//undefind
+var x;
+//null
+var namenum3 = null;
+document.write(namenum3);
+document.write("<br>");
+document.write(typeof namenum3);
+//boolean
+var k1=30,k2=50;
+document.write("<br>");
+document.write(k1 + k2 );
+
+//assignment operator
+var x = 20;
+var result= x += 4;
+document.write(result);
+
+x += 4;// x = x+4;
+x -= 4;
+x *= 4;
+x /= 4;
+x %= 4;
